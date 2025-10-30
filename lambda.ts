646667267f7b98d671fbf8b35d930debbdd64be2
bao-anh <mgbanh97@gmail.com>
2025-10-30 14:17:01 +0700

    move lambda.ts to outer

---
+++ lambda.ts
@@ -0,0 +1,5 @@
+import { APIGatewayProxyHandler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
+import { app } from './src/app';
+
+export const handler: APIGatewayProxyHandler = serverlessExpress({ app });
--- src/lambda.js
+++
@@ -1,4 +0,0 @@
-import serverlessExpress from '@vendia/serverless-express';
-import app from './src/app.js';
-
-export const handler = serverlessExpress({ app });
