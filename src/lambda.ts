555126a8829ceb1bbeae65c86935def43c6f010d
bao-anh <mgbanh97@gmail.com>
2025-10-30 14:14:35 +0700

    change from ts to js in lambda

--- src/lambda.ts
+++
@@ -1,5 +0,0 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import serverlessExpress from '@vendia/serverless-express';
-import { app } from './app';
-
-export const handler: APIGatewayProxyHandler = serverlessExpress({ app });
