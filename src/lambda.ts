ccdd92ada58fb621a29cd4891e25f3419c667fb9
bao-anh <mgbanh97@gmail.com>
2025-10-30 17:33:05 +0700

    fix nothing

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,6 +1,9 @@
 import serverlessExpress from '@vendia/serverless-express';
 import { app } from './app';
 
-export const handler = serverlessExpress({ app });
+export const handler = serverlessExpress({
+  app,
+  eventSourceName: 'AWS_API_GATEWAY_V2',
+});
 
 export default handler;
