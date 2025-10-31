bc60bdc52462d8e2764b58cb15f17d9028b09012
bao-anh <mgbanh97@gmail.com>
2025-10-31 07:59:06 +0700

    update dynamoDB client

--- src/config/dynamodb.ts
+++ src/config/dynamodb.ts
@@ -6,11 +6,11 @@ dotenv.config();
 
 const client = new DynamoDBClient({
   region: process.env.AWS_REGION || 'ap-southeast-1',
-  endpoint: 'http://localhost:8000',
+  endpoint: process.env.DYNAMODB_ENDPOINT || 'http://localhost:8000',
   credentials: {
-    accessKeyId: 'myAccessKey',
-    secretAccessKey: 'mySecretKey'
-  }
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'myAccessKey',
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || 'mySecretKey',
+  },
 });
 
 export const dynamoDb = DynamoDBDocumentClient.from(client);
